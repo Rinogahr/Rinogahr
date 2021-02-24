63cfccfb9946341735faa521a8ca784ea849f772
Rodrigo Maciel <69148290+Rinogahr@users.noreply.github.com>
2021-02-24 00:48:23 -0300

    aula de react js

---
+++ Trilha_RactJSNLW/moveit/src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from './componentes/Button';
+function App() {
+  return (
+    <div>
+      <Button color="red">
+        Rodrigo
+      </Button>
+      <Button color="blue">
+        João
+      </Button>
+      <Button color="pink">
+        Zaminha
+      </Button>
+    </div>
+  );
+}
+
+export default App;
